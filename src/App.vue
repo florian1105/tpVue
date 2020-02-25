88a66f134d080a44013d877a111e636b184922d0
florian1105 <43952610+florian1105@users.noreply.github.com>
2020-02-25 15:27:58 +0100

    first components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <div id="app">
-    <img width="25%" src="./assets/logo.png">
-    <HelloWorld msg="Hello Vue in CodeSandbox!" />
-  </div>
+<div id="app">
+  <img width="25%" src="./assets/photo.jpg" id="imgAcc">
+  <Header msg="Welcome to my PortFolio"></Header>
+  <body>
+    <FirstBody/>
+    <SecBody/>
+  </body>
+  <Footer msg="Footer"/>
+</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import Header from "./components/base/Header";
+import Footer from "./components/base/Footer";
+import FirstBody from "./components/base/body/firstBody";
+import SecBody from "./components/base/body/secBody";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Header,
+    Footer,
+    FirstBody,
+    SecBody
   }
 };
 </script>
@@ -24,5 +35,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#imgAcc {
+  border-radius: 50%;
 }
 </style>
---
+++ src/components/base/Footer.vue
@@ -0,0 +1,39 @@
+
+<template>
+  <div class="footer">
+    <h3>{{ msg }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+#footer {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+</style>
